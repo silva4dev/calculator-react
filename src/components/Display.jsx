f5602ccc1c7179b6ccd7ec12bd72bf8cd6befdc9
Lucas Alves da Silva <web4dev.lucas@outlook.com>
2022-10-13 04:41:02 -0300

    inicial

---
+++ src/components/Display.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Display.css'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => 
+        <div className="display">
+            {props.value}
+        </div>
